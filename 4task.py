a8126f7e403a6be3c59f672f7560758723b6fffd
NEVIL <nevilj22@gmail.com>
2025-07-12 17:15:22 +0530

    4task.py

--- 4task.py
+++ 4task.py
@@ -17,3 +17,24 @@ count = 'a'
 while count < 'h':
     print("Count is:", count)
     count = chr(ord(count) + 1)
+    
+    #task number
+# Expected: Print only odd numbers from 1 to 20
+for i in range(1, 21):
+    if i % 2 == 0:
+        continue
+    print(i)
+
+#PASSOWORD
+password = ""
+while password != "NEVIL123":
+    password = input("Enter password: ")
+print("Access granted!")
+
+#WORD COUNT
+# Expected: Only names with >5 characters and containing 'a' (case-insensitive)
+names = ["Alice", "JonAthAn", "Bob", "AnAstAsiA", "Sam", "Carla", "Ben"]
+
+for name in names:
+    if len(name) > 5 and 'a' in name.lower():
+        print(name)
