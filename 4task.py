1d28fba27cc70a35abf234c2a1d19da4c1733331
NEVIL <nevilj22@gmail.com>
2025-07-12 16:45:45 +0530

    4task.py

---
+++ 4task.py
@@ -0,0 +1,19 @@
+# for and while loop
+for i in range(5):
+    print(i)
+    print("loop",i)
+    
+# while loop numbers
+
+count = 0
+while count < 5:
+    print("Count is:", count)
+    count += 1
+    print()
+#variables
+
+count = 'a'
+
+while count < 'h':
+    print("Count is:", count)
+    count = chr(ord(count) + 1)
