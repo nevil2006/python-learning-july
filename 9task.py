f99910c85c135095069053980415f3ca4ce6017b
NEVIL <nevilj22@gmail.com>
2025-07-18 18:45:00 +0530

    9task.py

--- 9task.py
+++ 9task.py
@@ -11,3 +11,28 @@ while student_count > 0:
         print("Student count is:", student_count)
 
 print("No more students left.")
+#break
+for i in range(1,10):
+    if i==5:
+        break
+    print(i)
+#continue
+for i in range(1,10):
+    if i==5:
+        continue
+    print(i)
+#range type1
+for i in range(5):
+    print(i)
+#range type2
+for i in range(1,5):
+    print(i)
+#range type3
+for i in range(1,10,1):
+    print(i)
+#even number
+for i in range(1, 10, 1):
+    if i % 2 == 0:
+        print(i)
+    else:
+        continue  
