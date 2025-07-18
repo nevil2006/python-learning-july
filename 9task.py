ed92b8b36705f074ed40c54af3e843a53cb9d0be
NEVIL <nevilj22@gmail.com>
2025-07-18 09:51:50 +0530

    9task.py

---
+++ 9task.py
@@ -0,0 +1,13 @@
+#for loop
+name =["madhan","perusu","mama"]
+for studnets in name:
+    print(studnets)
+#while loop
+student_count = 190
+
+while student_count > 0:
+    student_count -= 62
+    if student_count > 0:
+        print("Student count is:", student_count)
+
+print("No more students left.")
