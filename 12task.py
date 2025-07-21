9c9727657391125c93e9c11a80273ef90155acae
NEVIL <nevilj22@gmail.com>
2025-07-21 19:14:00 +0530

    12task.py

--- 12task.py
+++ 12task.py
@@ -11,3 +11,9 @@ for idx, val in enumerate(vegetables):
 marks=[90, 85, 78, 92]
 print(all(m > 50 for m in marks))  
 print(any(m > 60 for m in marks))  
+#sorted():
+fruits=["banana", "apple", "cherry", "date"]
+print(sorted(fruits,key=len))
+#list comprehension:
+squared = [x*x for x in range(1,11)]
+print(squared)
