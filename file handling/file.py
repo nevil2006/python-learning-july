c183ee1d8f488b47977f53bf07661ccb5b75697c
NEVIL <nevilj22@gmail.com>
2025-07-22 18:53:17 +0530

    Update file.py with new write operations

--- file handling/file.py
+++ file handling/file.py
@@ -2,3 +2,18 @@
 file=open(r'C:\Users\nevil\Desktop\python-learning-july\file handling\info.txt','r')
 print(file.read())
 file.close()
+#read line
+print(file.readline())
+
+file.close()    
+#write
+file=open(r'C:\Users\nevil\Desktop\python-learning-july\file handling\info.txt','w')
+file.write('This is a new line added to the file.')
+file.close()
+#append
+file=open(r'C:\Users\nevil\Desktop\python-learning-july\file handling\info.txt','a')
+
+file.write('This line is appended to the file.')
+lst=['hi hi hi',"welcome to code.io"]
+file.writelines()
+file.close()
